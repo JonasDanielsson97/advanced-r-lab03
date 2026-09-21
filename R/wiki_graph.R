de9569a0ad9684a7c6824a860513df424c041d88
Jonas Danielsson <jonda228@student.liu.se>
2026-09-21 02:08:03 +0200

    Add documentation wiki_graph

---
+++ R/wiki_graph.R
@@ -0,0 +1,11 @@
+#' wiki_graph dataset
+#'
+#' This dataset is a sample graph used for testing the Dijkstra algorithm implementation. It contains nodes and edges with weights, representing a simple directed graph.
+#'
+#' @format A data frame with 3 columns:
+#' \describe{
+#'   \item{v1}{From node (integer).}
+#'   \item{v2}{To node (integer).}
+#'   \item{w}{Weight of the edge (numeric).}
+#' }
+"wiki_graph"
